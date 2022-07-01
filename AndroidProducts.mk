f335d4604276e84ebf8df55bf42c863bd035f446
Imtiaz Modder <67188507+Imtiaz-Modder@users.noreply.github.com>
2022-07-01 20:58:35 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/aosp_lava.mk
+   $(LOCAL_DIR)/corvus_lava.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_lava-user \
-    aosp_lava-userdebug \
-    aosp_lava-eng
+    corvus_lava-user \
+    corvus_lava-userdebug \
+    corvus_lava-eng
